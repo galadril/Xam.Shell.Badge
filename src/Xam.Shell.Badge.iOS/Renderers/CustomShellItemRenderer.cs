dd9b1c254bd71ae8ee50f93df3daaf199586cc84
Nikita Ponomarenko <superprojects@live.ru>
2020-10-23 02:02:19 +0600

    - iOS feature complete;

--- src/Xam.Shell.Badge.iOS/Renderers/CustomShellItemRenderer.cs
+++ src/Xam.Shell.Badge.iOS/Renderers/CustomShellItemRenderer.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using UIKit;
 using Xamarin.Forms;
@@ -7,14 +7,30 @@ using Xamarin.Forms.Platform.iOS;
 
 namespace Xam.Shell.Badge.iOS.Renderers
 {
+    /// <summary>
+    /// Defines the <see cref="CustomShellItemRenderer" />.
+    /// </summary>
     public class CustomShellItemRenderer : ShellItemRenderer
     {
+        #region Constructor & Destructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomShellItemRenderer"/> class.
+        /// </summary>
+        /// <param name="context">The shellContext<see cref="IShellContext"/>.</param>
         public CustomShellItemRenderer(IShellContext context) : base(context) { }
 
-        protected override void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            base.OnItemsCollectionChanged(sender, e);
+        #endregion
 
+        #region Public
+
+        /// <summary>
+        /// Occures when view is about to appear
+        /// </summary>
+        /// <param name="animated"></param>
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
             for (int i = 0; i < ShellItem.Items.Count; i++)
             {
                 var item = ShellItem.Items.ElementAtOrDefault(i);
@@ -25,18 +41,49 @@ namespace Xam.Shell.Badge.iOS.Renderers
             }
         }
 
+        #endregion
+
+        #region Protected
+
+        /// <summary>
+        /// Occures when property changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnShellSectionPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnShellSectionPropertyChanged(sender, e);
+
+            if (e.PropertyName == Badging.BadgeTextProperty.PropertyName ||
+                e.PropertyName == Badging.BadgeTextColorProperty.PropertyName ||
+                e.PropertyName == Badging.BadgeBackgroundColorProperty.PropertyName)
+            {
+                var item = (ShellSection)sender;
+                var index = ShellItem.Items.IndexOf(item);
+                var text = Badging.GetBadgeText(item);
+                var textColor = Badging.GetBadgeTextColor(item);
+                var bg = Badging.GetBadgeBackgroundColor(item);
+                if (!string.IsNullOrEmpty(text))
+                    ApplyBadge(index, text, bg, textColor);
+            }
+        }
+
+        #endregion
+
+        #region Private
+
         private void ApplyBadge(int index, string text, Color bg, Color textColor)
         {
-            if(TabBarController?.TabBar?.Items != null && TabBarController.TabBar.Items.Any())
+            if (TabBar?.Items != null && TabBar.Items.Any())
             {
                 if (!string.IsNullOrEmpty(text))
                 {
                     var badgeValue = Convert.ToInt32(text);
                     if (badgeValue > 0)
                     {
-                        TabBarController.TabBar.Items[index].BadgeValue = text;
-                        TabBarController.TabBar.Items[index].BadgeColor = bg.ToUIColor();
-                        TabBarController.TabBar.Items[index]
+                        TabBar.Items[index].BadgeValue = text;
+                        TabBar.Items[index].BadgeColor = bg.ToUIColor();
+                        TabBar.Items[index]
                             .SetBadgeTextAttributes(new UIStringAttributes()
                             {
                                 ForegroundColor = textColor.ToUIColor()
@@ -44,9 +91,9 @@ namespace Xam.Shell.Badge.iOS.Renderers
                     }
                     else
                     {
-                        TabBarController.TabBar.Items[index].BadgeValue = "●";
-                        TabBarController.TabBar.Items[index].BadgeColor = UIColor.Clear;
-                        TabBarController.TabBar.Items[index]
+                        TabBar.Items[index].BadgeValue = "●";
+                        TabBar.Items[index].BadgeColor = UIColor.Clear;
+                        TabBar.Items[index]
                             .SetBadgeTextAttributes(new UIStringAttributes()
                             {
                                 ForegroundColor = textColor.ToUIColor()
@@ -55,10 +102,12 @@ namespace Xam.Shell.Badge.iOS.Renderers
                 }
                 else
                 {
-                    TabBarController.TabBar.Items[index].BadgeValue = null;
-                    TabBarController.TabBar.Items[index].BadgeColor = UIColor.Clear;
+                    TabBar.Items[index].BadgeValue = null;
+                    TabBar.Items[index].BadgeColor = UIColor.Clear;
                 }
             }
         }
+
+        #endregion
     }
 }
